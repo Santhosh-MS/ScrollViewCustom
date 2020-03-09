b257545cfb330cf6d63cc8125a55956ad9a23d2c
Priya <idealab@imacs-imac.local>
2020-03-09 14:54:23 +0530

    Initial Commit

---
+++ ScrollView/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ScrollView
+//
+//  Created by Priya on 3/9/20.
+//  Copyright © 2020 ZSL. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
